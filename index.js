5a28e76359dc1edcdca4812afdf65d3eaf13fcac
Bamigbele Isaiah Ayomide <aryomi5528@gmail.com>
2025-10-22 17:01:18 +0100

    Poster section - music poster, name and artiste name fetched from an API

---
+++ index.js
@@ -0,0 +1,32 @@
+const songPreviews = document.querySelector(".songsPreview");
+
+async function getMusic() {
+  try {
+    const response = await axios.get("https://itunes.apple.com/search?", {
+      params: { term: "afrobeats", media: "music", limit: 4 },
+    });
+    const songs = response.data.results;
+    displaySong(songs);
+  } catch (error) {
+      songPreviews.innerHTML = `<p>Something went wrong. Try again later.</p>`;
+    console.error(error);
+  }
+}
+getMusic();
+
+function displaySong(songs) {
+  if (songs.length === 0)
+    return (songDiv.innerHTML = `<p>CONTENT NOT FOUND PLEASE TRY AGAIN LATER</p>`);
+  songs.forEach(({ collectionName, artistName, artworkUrl100 }) => {
+    const songName = collectionName;
+    imageUrl = artworkUrl100;
+    const songDiv = document.createElement("div");
+    songDiv.classList.add("songContainer");
+    songDiv.innerHTML = `
+        <img src="${imageUrl}" class="imagePoster" alt="music cover art">
+        <h4>${songName}</h4>
+        <p>${artistName}</p>
+        `;
+    songPreviews.appendChild(songDiv);
+  });
+}
